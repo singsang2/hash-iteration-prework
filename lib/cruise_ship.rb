3b3c38e3b0d31291ee4c8679888a1b7e4ecdb0a5
Sung Bae <singsang2@gmail.com>
2018-10-08 01:23:52 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,6 +7,7 @@ suite_e: "Crumpet the Elf"
 }
 
 def select_winner(passengers)
+  winner = ""
   passengers.each do |suite, name|
     if suite==:suite_a && name.start_with?("A")
       winner = name
